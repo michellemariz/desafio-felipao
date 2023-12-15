ddacd230727df30ec077bc25b55c7dd382765363
michellemariz <michellebarros82@gmail.com>
2023-12-15 16:44:51 -0300

    adicionei comandos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-let nome = "felipe"
-console.log(nome)
+let nome = "O Heroi de nome Mr Mármore"
 
+let nívelXp = 10.100
+
+if(nívelXp<1.000){
+    console.log((nome)+(" está no nível de Ferro"))
+}else if(nívelXp>=1.001 && nívelXp<=2.000){
+    console.log((nome)+(" está no nível de Bronze"))
+
+}else if(nívelXp>=2.001 && nívelXp<=6.000){
+    console.log((nome)+(" está no nível de Prata"))
+
+}else if(nívelXp>=6.001 && nívelXp<=7.000){
+    console.log((nome)+(" está no nível de Ouro"))
+
+}else if(nívelXp>=7.001 && nívelXp<=8.000){
+    console.log((nome)+("está no nível de Platina"))
+
+}else if(nívelXp>=8.001 && nívelXp<=9.000){
+    console.log((nome)+(" está no nível de Ascendente"))
+
+}else if(nívelXp>=9.001 && nívelXp<=10.000){
+    console.log((nome)+(" está no nível de Imortal"))
+
+
+}else if(nívelXp>=10.000){
+    console.log((nome)+(" está no nível de Radiante"))
+}
 
 
